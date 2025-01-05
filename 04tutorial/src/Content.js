180286afeceeb1c32b005b15851ab906f013cd3b
oickle <oycell@aol.com>
2025-01-05 00:18:59 -0500

    Complete Chapter 4

---
+++ 04tutorial/src/Content.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Content = () => {
+    // Gives a random name from list of 3 names!
+    const handleNameChange = () => {
+        const names = ['Bob', 'Kevin', 'Stuart'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    }
+
+    return (
+        <main>
+            Hello {handleNameChange()}!
+        </main>
+    )
+}
+
+export default Content
